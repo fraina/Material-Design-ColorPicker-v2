e5e5858d20ea4c5090d55756d75f72ff7b3acaf5
Fraina <jscaem@gmail.com>
2015-02-09 18:26:07 +0800

    復原 toolbar.jsx

--- src/js/views/toolbar.jsx
+++ src/js/views/toolbar.jsx
@@ -12,20 +12,37 @@
 ) {
   'use strict';
 
-  var ToolBarLocker = React.createClass({
+
+  var ToolBarInfo = React.createClass({
     clickHandler: function() {
-      var $locker = $('.flaticon-locker')
-      $locker.toggleClass('is-locked');
+      var $info = $('.flaticon-list'),
+          $picker_colors = $('.picker').find('.picker-colors');
+      $info.toggleClass('is-showInfo');
+      $picker_colors.toggleClass('show-info');
     },
 
     render: function() {
-      return (<i className="flaticon-locker" onClick={this.clickHandler}></i>)
+      return (<i className="flaticon-list" onClick={this.clickHandler}></i>)
+    }
+  })
+
+  var ToolBarFilter = React.createClass({
+    clickHandler: function() {
+      var $locker = $('.flaticon-filter')
+      $locker.toggleClass('is-filtering');
+    },
+
+    render: function() {
+      return (<i className="flaticon-filter is-filtering" onClick={this.clickHandler}></i>)
     }
   })
 
   var ToolBarTrash = React.createClass({
-    clickHandler: function() {
-      this.props.pickedCollect.reset();
+    clickHandler: function(e) {
+      var $tar = $(e.target),
+          $clearAll = $('.picker-color--clearAll');
+      $tar.toggleClass('is-deletable');
+      $clearAll.toggleClass('dn');
     },
 
     render: function() {
@@ -50,8 +67,9 @@
       return (
         <div className="toolbar" onClick={this.clickHandler}>
           <i className="flaticon-note"></i>
-          <ToolBarLocker />
+          <ToolBarInfo />
           <ToolBarTrash pickedCollect={pickedCollect}/>
+          <ToolBarFilter />
           <ul className="ghbtns">
             <li><iframe src="http://ghbtns.com/github-btn.html?user=Fraina&repo=Material-Design-ColorPicker-v2&type=watch&count=true" allowtransparency="true" frameborder="0" scrolling="0" width="110" height="20"></iframe></li>
             <li><iframe src="http://ghbtns.com/github-btn.html?user=Fraina&repo=Material-Design-ColorPicker-v2&type=fork&count=true" allowtransparency="true" frameborder="0" scrolling="0" width="110" height="20"></iframe></li>
